ceb90a631484ea72fb8e949ac8278a42cf279e68
Nikita Kuznetsov <n.kuznetsov@iconic.vc>
2017-11-29 03:26:01 +0300

    test 0pdd integration #2

---
+++ test.java
@@ -0,0 +1,16 @@
+/**
+ * @todo #234:15m/DEV This is something to do later
+ *  in one of the next releases. I can't figure out
+ *  how to implement it now, that's why the puzzle.
+ */
+void sendEmail() {
+  throw new UnsupportedOperationException();
+}
+
+
+/**
+ * @todo #1:15m/DEV Should did this task
+ */
+void getEmail() {
+  throw new UnsupportedOperationException();
+}
